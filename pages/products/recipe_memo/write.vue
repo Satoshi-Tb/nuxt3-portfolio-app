795d5075f1ec3c3e7f001e23995441b93fd72bcd
Satoshi-Tb <sulp91_fy@yahoo.co.jp>
2022-10-10 23:18:17 +0000

    DB初期化処理追加

--- pages/products/recipe_memo/write.vue
+++ pages/products/recipe_memo/write.vue
@@ -4,6 +4,7 @@ import RecipeForm from '~~/components/RecipeForm.vue';
 import { v4 } from "uuid";
 import { RecipeEntity } from "~~/types/RecipeEntity";
 
+//reactiveで値が変わるごとに再描画する「状態」を定義する
 const form = reactive<RecipeEntity>({
     name: "",
     items: [
@@ -16,14 +17,22 @@ const form = reactive<RecipeEntity>({
     howToCook: "",
 });
 
-const dummy = () => {
-    alert("工事中");
-};
+const dbName = "recipe-memo";
+const tableName = "recipe";
+//mountedライフサイクルフック
+//サーバで初期化を終えたコンポーネントが、ブラウザ上に組み込まれたタイミングで実施される
+onMounted(() => {
+    //IndexedDBにテーブルを作成
+    const openRequest = indexedDB.open(dbName);
+    openRequest.onupgradeneeded = (evt) => {
+        const db = (evt.target as IDBRequest).result;
+        db.createObjectStore(tableName, {keyPath: "id"});
+    };
+});
 
-const dummyS = ():string => {
-    return "dummy";
-};
-
+const goBack = () => {
+    navigateTo("/products/recipe_memo");
+}
 
 </script>
 <template>
@@ -31,10 +40,9 @@ const dummyS = ():string => {
         <div>
             <AppH2>レシピをメモできるアプリ</AppH2>            
             <div class="text-right w-full">
-                <ButtonSecondary v-bind:onClick="dummy">
+                <ButtonSecondary v-bind:onClick="goBack">
                     アプリTOPに戻る
                 </ButtonSecondary>
-
             </div>
         </div>
         <AppH3>レシピを書く</AppH3>
