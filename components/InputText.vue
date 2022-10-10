aac2ae3ad90f3055b61c67495b55aa69abbb7ace
Satoshi-Tb <sulp91_fy@yahoo.co.jp>
2022-10-10 10:32:55 +0000

    入力コンポーネント作成

--- components/InputText.vue
+++ components/InputText.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+//Vueは双方向バインディングモデル（Reactは単方向モデル）
+
+//親→子への情報伝達用
 const props = defineProps<{modelValue: string}>();
+
+//子→親への情報伝達用
 const emit = defineEmits(["update:modelValue"]);
+
+//v-modelタグに指定することでバインド。JavaScript変数とフォームの値を連結できる。
+//v-modelは、v-dind:modelValueと、@update:modelValueのシンタックスシュガー
 const modelValue = computed({
     get: () => props.modelValue,
-    set: (value) => emit("update:modelValue", value),
+    set: (value) => emit("update:modelValue", value), //第1引数はイベント名、第2引数は実際に親に渡す値
 });
 </script>
 <template>
