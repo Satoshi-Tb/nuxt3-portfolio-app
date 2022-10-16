6518aaf244215556dc49b98b9f4365fca8f48491
Satoshi-Tb <sulp91_fy@yahoo.co.jp>
2022-10-16 16:01:32 +0000

    一覧照会ページ追加

--- pages/products/recipe_memo/index.vue
+++ pages/products/recipe_memo/index.vue
@@ -1,6 +1,72 @@
+<script setup lang="ts">
+const links = ref<{
+    url: string;
+    text: string;
+}[]>([]);
+
+
+const dbName = "recipe-memo";
+const tableName = "recipe";
+
+onMounted(() => {
+    const openRequest = indexedDB.open(dbName);
+
+    openRequest.onsuccess = (evt) => {
+        const db = (evt.target as IDBRequest).result;
+        const transaction = db.transaction(tableName, "readonly");
+        const table = transaction.objectStore(tableName) as IDBObjectStore;
+
+        const cursorRequest = table.openCursor();
+
+        cursorRequest.onsuccess = (evt) => {
+            const cursor = (evt.target as IDBRequest).result;
+
+            //最後のレコードまで操作した場合、カーソルがnullになっているはず
+            if (!cursor) return;
+
+            const record = cursor.value;
+
+            links.value.push({
+                url: `/products/recipe_memo/${record.id}`,
+                text: record.name,
+            });
+
+            //次のカーソルに進める
+            //次のカーソルに進んだら、またonsuccessがコールバックされる
+            cursor.continue();
+        };
+    };
+
+    //アプリ内でこのページを最初に開くユースケースが中心のはずなので
+    //ここでもテーブル作成処理を挟んでおく
+    openRequest.onupgradeneeded = (evt) => {
+        const db = (evt.target as IDBRequest).result;
+        db.createObjectStore(tableName, {keyPath: "id"});
+    };
+
+});
+
+const goBack = () => {
+    //ポートフォリオサイトのトップに移動
+    return navigateTo("/");
+}
+
+const goWrite = () => {
+    return navigateTo("/products/recipe_memo/write");
+}
+</script>
 <template>
-    <div>
-        <h2>レシピをメモできるアプリ</h2>
-        <div>page/products/recipe_memo/index.vue</div>
-    </div>
+    <PageContainer>
+        <div>
+            <AppH2>レシピをメモできるアプリ</AppH2>
+            <div class="text-right w-full">
+                <ButtonSecondary v-bind:onClick="goBack">
+                サイトTOPに戻る
+                </ButtonSecondary>
+            </div>
+        </div>
+        <AppH3>レシピ一覧</AppH3>
+        <AppLink v-bind:links="links" />
+        <ButtonIconPlus v-bind:onClick="goWrite" />
+    </PageContainer>
 </template>
